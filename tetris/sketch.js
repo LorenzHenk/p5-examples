9ac311800dc25a7b30372f654ddff477fd7e87a5
Lorenz Henk <lorenz.henk@cybertec.at>
2022-03-27 13:37:11 +0200

    chore: add score to tetris

--- tetris/sketch.js
+++ tetris/sketch.js
@@ -32,34 +32,38 @@ const TILES = [
 let board;
 let level;
 let paused = false;
+let highscore = 0;
+let score = 0;
 
 function setup() {
   createCanvas(400, 600);
   board = new TetrisBoard();
   level = 1;
 
-  frameRate(level);
+  frameRate(1);
+  board.draw();
 }
 
 function draw() {
-  background(220);
+  background(0);
 
-  board.update();
+  if (!paused) {
+    board.update();
+    score++;
+  }
+
   board.draw();
 
-  frameRate(level);
+  frameRate(Math.log2(level + 1));
 }
 
 function keyPressed() {
-  board.keyPressed();
+  if (!paused) {
+    board.keyPressed();
+  }
 
   if (keyCode == 32) {
     paused = !paused;
-  }
-  if (paused) {
-    noLoop();
-  } else {
-    loop();
   }
 }
 
@@ -70,7 +74,7 @@ class TetrisBoard {
 
   reset() {
     this.board = Array.from({ length: ROW_COUNT }).map(() =>
-      Array.from({ length: COLUMN_COUNT }).map(() => null)
+      Array.from({ length: COLUMN_COUNT }).map(() => null),
     );
     this.tileIndex = 0;
     this.tile = TILES[this.tileIndex];
@@ -81,6 +85,10 @@ class TetrisBoard {
     };
     this.tileColorIndex = 0;
     level = 1;
+    if (score > highscore) {
+      highscore = score;
+    }
+    score = 0;
   }
 
   draw() {
@@ -106,10 +114,18 @@ class TetrisBoard {
           ((i % size) + this.tilePosition.x) * SIZE,
           (Math.floor(i / size) + this.tilePosition.y) * SIZE,
           SIZE,
-          SIZE
+          SIZE,
         );
       }
     }
+
+    this.drawScore();
+  }
+
+  drawScore() {
+    fill(255);
+    text(highscore + "", 100, 20);
+    text(score + "", 100, 40);
   }
 
   update() {
@@ -146,9 +162,10 @@ class TetrisBoard {
       if (this.board[i].every((x) => x)) {
         this.board.splice(i, 1);
         this.board.unshift(
-          Array.from({ length: COLUMN_COUNT }).map(() => null)
+          Array.from({ length: COLUMN_COUNT }).map(() => null),
         );
         level++;
+        score += 100;
       }
       i++;
     }
@@ -162,24 +179,28 @@ class TetrisBoard {
 
   keyPressed() {
     switch (keyCode) {
-      case 37: {
+      case 37:
+        {
           if (this.tileCanMoveLeft(this.tile, this.tilePosition)) {
             this.tilePosition.x--;
           }
         }
         break;
-      case 39: {
+      case 39:
+        {
           if (this.tileCanMoveRight(this.tile, this.tilePosition)) {
             this.tilePosition.x++;
           }
         }
         break;
-      case 38: {
+      case 38:
+        {
           const nextTile = this.rotateTile();
           this.tile = nextTile;
         }
         break;
-      case 40: {
+      case 40:
+        {
           if (this.tileCanMoveDown(this.tile, this.tilePosition)) {
             this.tilePosition.y++;
           }
@@ -195,7 +216,7 @@ class TetrisBoard {
     const tileHeight =
       tile.reduceRight(
         (acc, next, index) => acc || (next && Math.floor(index / size)),
-        undefined
+        undefined,
       ) + 1;
 
     if (tilePosition.y + tileHeight >= ROW_COUNT) {
@@ -220,7 +241,7 @@ class TetrisBoard {
     const tileHeight =
       tile.reduceRight(
         (acc, next, index) => acc || (next && Math.floor(index / size)),
-        undefined
+        undefined,
       ) + 1;
     const tileEnd =
       tile.reduce((acc, next, index) => {
@@ -254,7 +275,7 @@ class TetrisBoard {
     const tileHeight =
       tile.reduceRight(
         (acc, next, index) => acc || (next && Math.floor(index / size)),
-        undefined
+        undefined,
       ) + 1;
 
     const leftStart = tile.reduce((acc, next, index) => {
@@ -286,7 +307,7 @@ class TetrisBoard {
   rotateTile() {
     const size = Math.sqrt(this.tile.length);
     const nextTile = Array.from({ length: size }).map(() =>
-      Array.from({ length: size }).map(() => 0)
+      Array.from({ length: size }).map(() => 0),
     );
 
     for (let cellIndex = 0; cellIndex < this.tile.length; cellIndex++) {
@@ -303,7 +324,11 @@ class TetrisBoard {
   newTile() {
     this.tileIndex = Math.floor(Math.random() * TILES.length);
     this.tile = TILES[this.tileIndex];
-    this.tilePosition = { x: 0, y: 0 };
+    const size = Math.sqrt(this.tile.length);
+    this.tilePosition = {
+      x: Math.floor(Math.random() * (COLUMN_COUNT - size)),
+      y: 0,
+    };
     this.tileColorIndex = (this.tileColorIndex + 1) % COLORS.length;
   }
 }
